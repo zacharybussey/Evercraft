b8ba101cd8ee7efd0999e8123f1962456ed31111
Zachary Bussey <zacharybussey@gmail.com>
2016-10-19 13:03:45 -0400

    Progressed up through Character can Attack

--- src/EverCraft.elm
+++ src/EverCraft.elm
@@ -1,5 +1,29 @@
 module EverCraft exposing (..)
 
 
-hello =
-    "hello world"
+hello : String -> String
+hello thing =
+    "hello " ++ thing
+
+
+type alias Character =
+    { name : String
+    , alignment : Alignment
+    , armorClass : Int
+    , hitPoints : Int
+    }
+
+
+type Alignment
+    = Good
+    | Evil
+    | Neutral
+
+
+defaultCharacter =
+    { name = "", alignment = Neutral, armorClass = 10, hitPoints = 5 }
+
+
+attack : Int -> Int -> Bool
+attack diceRoll armorClass =
+    diceRoll >= armorClass
--- tests/Tests.elm
+++ tests/Tests.elm
@@ -8,16 +8,48 @@ import EverCraft exposing (..)
 all : Test
 all =
     describe "EverCraft"
-        [ testHello ]
+        [ testCoreBasics ]
 
 
-testHello =
+testCoreBasics =
     let
-        output =
-            "hello world"
+        character =
+            { defaultCharacter | name = "name", alignment = Good, armorClass = 10, hitPoints = 5 }
     in
-        describe "hello"
-            [ test "says hello world"
+        describe "Character"
+            [ test "has a name"
                 <| \() ->
-                    hello |> Expect.equal output
+                    (character.name |> Expect.equal "name")
+            , test "has an alignment"
+                <| \() ->
+                    (character.alignment |> Expect.equal Good)
+            , test "can set armor class"
+                <| \() ->
+                    let
+                        tougherCharacter =
+                            { character | armorClass = 11 }
+                    in
+                        (tougherCharacter.armorClass |> Expect.equal 11)
+            , test "can set hitPoints"
+                <| \() ->
+                    let
+                        hurtCharacter =
+                            { character | hitPoints = 3 }
+                    in
+                        (hurtCharacter.hitPoints |> Expect.equal 3)
             ]
+
+
+testAttacks =
+    describe "Attacking"
+        [ test "check for hit"
+            <| \() ->
+                let
+                    dieValue =
+                        20
+
+                    armorClass =
+                        10
+                in
+                    (attack dieValue armorClass) |> Expect.equal True
+        ]
